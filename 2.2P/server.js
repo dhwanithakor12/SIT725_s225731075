ec27a601c3f60e8cd780ca6978974c23f301749f
dhwanithakor12 <s225731075@deakin.edu.au>
2025-12-01 23:57:50 +0530

    updated project files

--- 2.2P/server.js
+++ 2.2P/server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+app.get("/add", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    return res.json({ error: "Both a and b must be numbers" });
+  }
+
+  const result = a + b;
+  res.json({ result: result });
+});
+app.get("/subtract", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.json({ result: a - b });
+});
